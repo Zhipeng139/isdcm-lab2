545ddb6508d6f6720b85c84ee93b20602cd66757
Zhipeng Lin <zhipeng139@gmail.com>
2026-03-26 16:45:19 +0100

    add health check api

--- src/main/java/org/upc/student/isdcm/entrega2/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package org.upc.student.isdcm.entrega2;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
---
+++ src/main/java/org/upc/student/isdcm/entrega2/controller/HealthResource.java
@@ -0,0 +1,23 @@
+package org.upc.student.isdcm.entrega2.controller;
+
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/health")
+public class HealthResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response checkHealth() {
+        JsonObject jsonPayload = Json.createObjectBuilder()
+                .add("status", "true")
+                .build();
+
+        return Response.ok(jsonPayload).build();
+    }
+}
