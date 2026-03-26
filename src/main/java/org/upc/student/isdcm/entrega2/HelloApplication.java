04518c66d553eac2a13046e70acc5ccb1cd66f31
Zhipeng Lin <zhipeng139@gmail.com>
2026-03-26 16:19:13 +0100

    first commit

---
+++ src/main/java/org/upc/student/isdcm/entrega2/HelloApplication.java
@@ -0,0 +1,9 @@
+package org.upc.student.isdcm.entrega2;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
